7636835c931f0514d94ffc26c30fbdbb5cee8cba
Biang0 <109459544+Biang0@users.noreply.github.com>
2025-03-25 11:45:30 +0800

    Update bilibili.py

--- core/bilibili.py
+++ core/bilibili.py
@@ -25,10 +25,10 @@ class Bilibili:
         return config.COIN_OR_NOT and coin_num >= 5
 
     def insert_coins(self) -> int:
-        video_list = BilibiliHttp.get_video_list(self.ck,config.UID_LIST)
+        video_list = BilibiliHttp.get_video_list(self.ck, config.UID_LIST)
         bvid, title, author, aid = BilibiliUtils.random_video_para(video_list)
         self.logger.info(f'开始向{author}的视频{title}投币……')
-        coin_res = BilibiliHttp.insert_coin(self.ck,aid,config.LIKE_OR_NOT)
+        coin_res = BilibiliHttp.insert_coin(self.ck, aid, config.LIKE_OR_NOT)
         return coin_res
 
     @staticmethod
@@ -48,8 +48,9 @@ class Bilibili:
         self.logger.info(f'本次投币任务数量:{insert_num}')
         success_count = 0
         fail_count = 0
+        total_count = 0
         if strict_mode:
-            while 1:
+            while total_count < insert_num:
                 coin_res = self.insert_coins()
                 if success_count == 5 or fail_count == 5:
                     break
@@ -57,28 +58,28 @@ class Bilibili:
                     success_count += 1
                 else:
                     fail_count += 1
+                total_count += 1
                 self.logger.info(f'当前投币成功{success_count},失败{fail_count}次')
                 sleep(1)
         else:
             for x in range(0, insert_num):
                 self.insert_coins()
                 sleep(1)
-        self.logger.info(f'每日投币:完成~获得{insert_num * 10}点经验值')
+        self.logger.info(f'每日投币:完成~获得{min(total_count, insert_num) * 10}点经验值')
 
     def do_share_video(self):
-        video_list = BilibiliHttp.get_video_list(self.ck,config.UID_LIST)
+        video_list = BilibiliHttp.get_video_list(self.ck, config.UID_LIST)
         bvid, title, author, aid = BilibiliUtils.random_video_para(video_list)
         self.logger.info(f'开始分享{author}的视频{title}……')
-        BilibiliHttp.share_video(self.ck,bvid)
+        BilibiliHttp.share_video(self.ck, bvid)
         self.logger.info('分享视频任务完成～')
 
     def do_watch_video(self):
-        video_list = BilibiliHttp.get_video_list(self.ck,config.UID_LIST)
+        video_list = BilibiliHttp.get_video_list(self.ck, config.UID_LIST)
         bvid, title, author, aid = BilibiliUtils.random_video_para(video_list)
         self.logger.info(f'开始观看{author}的视频{title}……')
-        BilibiliHttp.watch_video(self.ck,bvid)
+        BilibiliHttp.watch_video(self.ck, bvid)
         self.logger.info('观看视频任务完成～')
-
 
     def do_job(self) -> None:
         self.logger.start()
@@ -91,9 +92,9 @@ class Bilibili:
 
         self.do_watch_video() if tasks["watch"] else self.logger.info("观看视频任务已完成～")
         self.do_share_video() if tasks["share"] else self.logger.info("分享视频任务已完成～")
-        self.do_insert_coin(config.STRICT_MODE,tasks["coins"]) if tasks["coins"]!=50 else self.logger.info("投币任务已完成～")
+        self.do_insert_coin(config.STRICT_MODE, tasks["coins"]) if tasks["coins"] != 50 else self.logger.info("投币任务已完成～")
 
-        #活动已经失效
+        # 活动已经失效
         # self.logger.info('=========直播签到========')
         # live_sign_res = BilibiliHttp.live_sign(self.ck)
         # self.logger.info(live_sign_res)
@@ -108,10 +109,7 @@ class Bilibili:
         else:
             self.logger.info('银瓜子转换币:跳过~')
 
-
-
         self.logger.info('=========以下是个人信息=========')
         user_info = BilibiliHttp.get_user_info(self.ck)
         self.logger.info(user_info)
         self.logger.end()
-
