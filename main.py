a25c9649c305c174570681d98a94175ce0213578
Biang0 <109459544+Biang0@users.noreply.github.com>
2025-03-26 12:56:47 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,26 +2,21 @@
 Author: Wyatt1026
 Project Address: https://github.com/Wyatt1026/BilibiliDailyUp
 """
-from config import config
+
+
 from core.bilibili import Bilibili
+from config import config
 import os
 
 if __name__ == '__main__':
-    # 检查是否使用环境变量
     if config.USE_ENVIRONMENT_VARIABLE:
-        # 获取环境变量中的 COOKIES
         cookies = os.environ.get('COOKIES')
-        if cookies:
-            # 分割 COOKIES 字符串为列表
-            ck_list = cookies.split(",")
-        else:
-            print("未设置 COOKIES 这个环境变量，任务终止")
-            ck_list = []
+        ck_list = cookies.split(",")
     else:
-        # 从配置文件中获取 COOKIE 列表
         ck_list = config.COOKIE_LIST
-
-    # 遍历 COOKIE 列表，执行任务
+    
     for ck in ck_list:
         bilibili = Bilibili(ck)
-        bilibili.do_job()
+        bilibili.go() 
+        
+    
